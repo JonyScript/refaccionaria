3b0e453f1c780ff562c69f71b86002ae6543d23b
Christian Diaz <uriel_130@outlook.com>
2020-11-23 20:13:28 -0600

    xd

--- Refaccionaria/ClaseRegistroDeProductos.vb
+++ Refaccionaria/ClaseRegistroDeProductos.vb
@@ -149,7 +149,7 @@
         'capturado en la caja de textos txt_usuario de la pantalla
         'FrmUsuarios
         strSQL = "SELECT * FROM producto " &
-                 "WHERE (codigoProducto= '" & codigoProducto & "' and eliminado = 0) OR (nombreProducto = '" & nombreProducto & "' and codigoMarca = " & CMarca & " and eliminado = 0)"
+                "WHERE (codigoProducto= '" & codigoProducto & "' and eliminado = 0) OR (nombreProducto = '" & nombreProducto & "' and codigoMarca = " & CMarca & " and eliminado = 0)"
         consultaUnProducto = False
         xDT = xCnx.objetoDataAdapter(strSQL)
         If xDT.Rows.Count > 0 Then
